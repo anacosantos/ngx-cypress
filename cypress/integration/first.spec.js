99bed49f8d7815283bf105a3e0ae8ab73712405d
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-10 17:03:20 +0300

    Invoke command finished

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -124,7 +124,7 @@ describe('First suite', () => {
         })
     })
 
-    it.only('Test invoke command', () => {
+    it('Test invoke command', () => {
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -161,9 +161,18 @@ describe('First suite', () => {
            expect(classValue).to.contain('checked')
        }))
 
-        
-       
+    })   
 
+    it.only('Assert property', () => {
 
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then((input) => {
+            cy.wrap(input).click()        
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'May 17, 2021')
+        })      
     })
 })
