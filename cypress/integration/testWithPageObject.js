c1493e97bc9a91de55fc84b9cb50397cfbf12576
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-18 17:08:28 +0300

    start Page object

---
+++ cypress/integration/testWithPageObject.js
@@ -0,0 +1,12 @@
+
+
+describe('Test with Page Object', () => {
+
+    beforeEach('open the application', () =>{
+        cy.visit('/')
+    })
+
+    it('Verify navigations across the pages', () => {
+        
+    })
+})
