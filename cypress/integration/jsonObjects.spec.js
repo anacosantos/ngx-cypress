5914a966255d4e0ab1d01bbeeff29e6729478bd2
anacosantos <anacarolinaosantos85@gmail.com>
2021-05-24 19:52:34 +0300

    Create json objects

---
+++ cypress/integration/jsonObjects.spec.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress"/>
+
+describe('JSON objects', () =>{
+
+    it.only('JSON objects', () => {
+        cy.openHomePage()
+
+        const simpleObject = {"Key": "value", "Key2": "value2",}
+
+        const simpleArrayOfValues = ["one", "two", "three"] // has index and starts with zero
+
+        const arrayOfObjects = [{"Key": "value"}, {"Key2": "value2"}, {"Key3": "value3"}]
+
+        const typeOfData = {"string": "this is a string", "number": 10}
+
+        const mix = {
+            "FirstName": "Ana",
+            "LastName": "Santos",
+            "Age": 35,
+            "Students": [
+                {
+                    "FirstName": "Marcus",
+                    "LastName": "Menezes",
+                }, 
+                {
+                    "FirstName": "Luis",
+                    "LastName": "Manuel",
+                }
+            ]
+        }
+
+
+        //print result into the console
+        console.log(simpleObject.Key2) //or console.log(simpleObject["Key2"])
+
+        console.log(simpleArrayOfValues[1])  
+        
+        console.log(arrayOfObjects[2].Key3)
+
+        console.log(mix.Students[0].FirstName)
+        console.log(mix.Students[1].LastName)
+
+        const lastNameOfSecondStudent = mix.Students[1].LastName
+        
+        
+    })
+})
